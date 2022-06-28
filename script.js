5e81297ff0d910f20758f9d4e85811601a782a74
Marlleyck Nathan <marlleyck@hotmail.com>
2022-06-28 12:21:40 -0300

    update musicPlayer

---
+++ script.js
@@ -0,0 +1,46 @@
+let musica = document.querySelector('audio')
+
+let duracaoMusica = document.querySelector('.fim')
+
+duracaoMusica.textContent = Math.floor(musica.duration)
+
+function tocarMusica() {
+    musica.play()
+    document.querySelector('.botao-pause').style.display = 'block'
+    document.querySelector('.botao-play').style.display = 'none'
+}
+
+function pausarMusica() {
+    musica.pause()
+    document.querySelector('.botao-pause').style.display = 'none'
+    document.querySelector('.botao-play').style.display = 'block'
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress')
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
+    let tempoCorrido = document.querySelector('.inicio')
+    tempoCorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime))
+
+}
+
+function segundosParaMinutos(segundos) {
+    let campoSegundos = segundos % 60
+    let campoMinuto = Math.floor(segundos / 60)
+
+    if (campoSegundos < 10) {
+        campoSegundos  = '0'+campoSegundos
+    }
+
+    return campoMinuto+':'+campoSegundos
+}
+
+
+
+document.querySelector('.botao-play')
+    .addEventListener('click', tocarMusica)
+
+document.querySelector('.botao-pause')
+    .addEventListener('click', pausarMusica)
+
+musica.addEventListener('timeupdate', atualizarBarra)
