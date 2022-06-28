0611ab4a7b098a84b17d8032329736b5d7b23029
Marlleyck Nathan <marlleyck@hotmail.com>
2022-06-28 12:50:12 -0300

    pre terminado projeto

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 let musica = document.querySelector('audio')
+let indexMusica = 0
+
+let musicas = [
+    {titulo: 'Guitar Solo', artista: 'Marlleyck Nathan', src:'music/We Ride! - Reed Mathis.mp3',
+    img: 'img/rock.jpg'},
+    {titulo: 'Samba', artista: 'Bossa Nuev', src: 'music/Ella Vater - The Mini Vandals.mp3',
+    img: 'img/samba.jpg'},
+    {titulo: 'Piano', artista: 'Pianista', src: 'music/A Brand New Start - TrackTribe.mp3',
+    img: 'img/background.jpg'}
+]
+
+renderizarMusica(indexMusica)
+
+function renderizarMusica(index) {
+    musica.setAttribute('src', musicas[index].src)
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.innerHTML = musicas[index].titulo
+        nomeArtista.innerHTML = musicas[index].artista
+        imagem.src = musicas[index].img
+        duracaoMusica.innerHTML = segundosParaMinutos(Math.floor(musica.duration))
+    })
+}
 
 let duracaoMusica = document.querySelector('.fim')
 
@@ -6,7 +28,6 @@ let imagem = document.querySelector('img')
 let nomeMusica = document.querySelector('.descricao h2')
 let nomeArtista = document.querySelector('.descricao i')
 
-duracaoMusica.innerHTML = segundosParaMinutos(Math.floor(musica.duration))
 
 function tocarMusica() {
     musica.play()
@@ -48,3 +69,21 @@ document.querySelector('.botao-pause')
     .addEventListener('click', pausarMusica)
 
 musica.addEventListener('timeupdate', atualizarBarra)
+
+document.querySelector('.anterior')
+    .addEventListener('click', () => {
+        indexMusica--
+        if (indexMusica < 0) {
+            indexMusica = 0
+        }
+        renderizarMusica(indexMusica)
+    })
+
+document.querySelector('.prox')
+    .addEventListener('click', () => {
+        indexMusica++
+        if (indexMusica > 2) {
+            indexMusica = 0
+        }
+        renderizarMusica(indexMusica)
+    })
